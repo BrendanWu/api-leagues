14a5961beb61ed163cf229ad7587f16b4616148f
Brendan Wu <wubrendan7@gmail.com>
2021-06-24 23:53:13 -0400

    api docs

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "example",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "title": "Custom apiDoc browser title",
+  "url": "https://api.github.com/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-06-25T02:28:20.254Z",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
 process.on('SIGINFO', (code)=> {
 	console.log("Process pid", process.pid);
 });
 
-app.get("/", (req,res)=> {
-	res.send("Leagues API v1.0");
+
+
+app.use(express.static(path.join(__dirname,"/apidoc")))
+app.get("/api", (req,res)=> {
+	res.sendFile(path.join(__dirname,"/apidoc/index.html"));
+})
+
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup Invitation
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+app.get("/api/t", (req,res)=> {
+	res.send("hi")
 })
 
 
